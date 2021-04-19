d0090f1d2add9376565f9368a9981be488857775
Diana Clarke <diana.joan.clarke@gmail.com>
2021-04-19 12:12:58 -0600

    Remove all but simple plots for now

--- conbench/hacks.py
+++ conbench/hacks.py
@@ -1,19 +1,27 @@
+def get_case(tags):
+    case = [
+        (k, v)
+        for k, v in sorted(tags.items())
+        if not isinstance(v, int)
+        and v is not None
+        and k != "id"
+        and k != "name"
+        and k != "dataset"
+        and k != "source"
+        and k != "language"
+    ]
+    if "language" in tags:
+        case.append(("language", tags["language"]))
+    return [v for _, v in case]
+
+
 def set_display_name(benchmark):
     is_api = isinstance(benchmark, dict)
     tags = benchmark["tags"] if is_api else benchmark.case.tags
     name = tags.get("name") if is_api else benchmark.case.name
     if "name" not in tags:
         tags["name"] = name
-    case = [
-        v
-        for k, v in sorted(tags.items())
-        if not isinstance(v, int)
-        and v is not None
-        and k != "id"
-        and k != "source"
-        and k != "name"
-        and k != "suite"
-    ]
+    case = get_case(tags)
     if "suite" in tags:
         case = [name] + case
     name = ", ".join(case) if case else name
@@ -38,19 +46,9 @@ def sorted_data(benchmarks):
     data = []
     for benchmark in benchmarks:
         tags = benchmark["tags"]
-        items = [
-            (k, v)
-            for k, v in sorted(tags.items())
-            if not isinstance(v, int)
-            and v is not None
-            and k != "id"
-            and k != "name"
-            and k != "dataset"
-            and k != "source"
-        ]
-        row = [v for k, v in items]
-        row.append(benchmark["stats"]["mean"])
-        data.append(row)
+        case = get_case(tags)
+        case.append(benchmark["stats"]["mean"])
+        data.append(case)
 
     # Try to sort the cases better
     # unsorted: ['262144/0', '262144/1', '262144/10', '262144/2']
