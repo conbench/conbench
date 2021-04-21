46b824fe12f894da2bedf673b90ad389a1ab4d95
Diana Clarke <diana.joan.clarke@gmail.com>
2021-04-21 14:19:13 -0600

    Add run compare link

--- conbench/app/runs.py
+++ conbench/app/runs.py
@@ -1,19 +1,33 @@
+import flask as f
+
 from ..app import rule
 from ..app._endpoint import AppEndpoint
 from ..app._util import augment
+from ..app.benchmarks import RunMixin
 from ..config import Config
 
 
-class RunPlot(AppEndpoint):
-    def page(self, benchmarks):
+class RunPlot(AppEndpoint, RunMixin):
+    def page(self, benchmarks, baseline_run, contender_run):
+        compare_runs_url = None
+        if baseline_run and contender_run:
+            compare = f'{baseline_run["id"]}...{contender_run["id"]}'
+            compare_runs_url = f.url_for("app.compare-runs", compare_ids=compare)
+
         return self.render_template(
             "run.html",
             application=Config.APPLICATION_NAME,
             title="Run",
             benchmarks=benchmarks,
+            compare_runs_url=compare_runs_url,
         )
 
     def get(self, run_id):
+        contender_run, baseline_run = self.get_display_run(run_id), None
+        if contender_run:
+            parent = contender_run["commit"]["parent_sha"]
+            baseline_run = self.get_display_run(run_id)  # TODO: get baseline run
+
         benchmarks, response = self._get_benchmarks(run_id)
         if response.status_code != 200:
             self.flash("Error getting benchmarks.")
@@ -22,7 +36,7 @@ class RunPlot(AppEndpoint):
         for benchmark in benchmarks:
             augment(benchmark)
 
-        return self.page(benchmarks)
+        return self.page(benchmarks, baseline_run, contender_run)
 
     def _get_benchmarks(self, run_id):
         response = self.api_get("api.benchmarks", run_id=run_id)
