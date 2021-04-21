646b63041ca8c8a0f6b94ec34c141b4df652041a
diana <diana.joan.clarke@gmail.com>
2021-04-21 15:40:46 -0600

    Use GitHub API token (#37)

--- migrations/versions/662175f2e6c6_backfill_parent.py
+++ migrations/versions/662175f2e6c6_backfill_parent.py
@@ -5,6 +5,8 @@ Revises: 782f4533db71
 Create Date: 2021-04-21 09:23:28.589522
 
 """
+import os
+
 from alembic import op
 import requests
 
@@ -23,12 +25,18 @@ def upgrade():
     commit_table = Commit.__table__
     connection = op.get_bind()
 
+    token, session = os.getenv("GITHUB_API_TOKEN"), None
+    if token:
+        session = requests.Session()
+        session.headers = {"Authorization": f"Bearer {token}"}
+
     commits = connection.execute(
         commit_table.select().where(commit_table.c.parent == None)  # noqa
     )
+
     for commit in commits:
         url = f"https://api.github.com/repos/apache/arrow/commits/{commit.sha}"
-        response = requests.get(url)
+        response = session.get(url) if session else requests.get(url)
         parent = response.json()["parents"][0]["sha"]
         connection.execute(
             commit_table.update()
