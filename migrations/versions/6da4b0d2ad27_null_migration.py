1eec7687d74b4e82933890c989c0ae3f6e8067e4
Diana Clarke <diana.joan.clarke@gmail.com>
2021-05-14 09:02:32 -0600

    Rename null migration

---
+++ migrations/versions/6da4b0d2ad27_null_migration.py
@@ -0,0 +1,23 @@
+"""null migration
+
+Revision ID: 6da4b0d2ad27
+Revises: b7ffcaaeb240
+Create Date: 2021-05-11 10:31:22.260237
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "6da4b0d2ad27"
+down_revision = "b7ffcaaeb240"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    return
+
+
+def downgrade():
+    pass
