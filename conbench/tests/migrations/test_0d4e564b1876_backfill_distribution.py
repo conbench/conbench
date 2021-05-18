51114c94afd313b9c2d4d07634ef7120c6b5c709
Diana Clarke <diana.joan.clarke@gmail.com>
2021-05-18 12:07:17 -0600

    Backfill distribution, take 2

--- conbench/tests/migrations/test_0d4e564b1876_backfill_distribution.py
+++ conbench/tests/migrations/test_0d4e564b1876_backfill_distribution.py
@@ -13,7 +13,7 @@ this_dir = os.path.abspath(os.path.dirname(__file__))
 config_path = os.path.join(this_dir, "../../../alembic.ini")
 
 
-VALID_PAYLOAD_1 = {
+VALID_PAYLOAD = {
     "context": {
         "arrow_compiler_flags": "-fPIC -arch x86_64 -arch x86_64 -std=c++11 -Qunused-arguments -fcolor-diagnostics -O3 -DNDEBUG",
         "arrow_compiler_id": "AppleClang",
@@ -89,114 +89,18 @@ VALID_PAYLOAD_1 = {
         "dataset": "nyctaxi_sample",
         "file_type": "parquet",
         "input_type": "arrow",
-    },
-}
-
-VALID_PAYLOAD_2 = {
-    "context": {
-        "arrow_compiler_flags": "-fPIC -arch x86_64 -arch x86_64 -std=c++11 -Qunused-arguments -fcolor-diagnostics -O3 -DNDEBUG",
-        "arrow_compiler_id": "AppleClang",
-        "arrow_compiler_version": "11.0.0.11000033",
-        "arrow_version": "2.0.0",
-        "benchmark_language_version": "Python 3.8.5",
-        "benchmark_language": "Python",
-    },
-    "github": {
-        "commit": "02addad336ba19a654f9c857ede546331be7b631",
-        "repository": "https://github.com/apache/arrow",
-    },
-    "machine_info": {
-        "architecture_name": "x86_64",
-        "cpu_l1d_cache_bytes": "32768",
-        "cpu_l1i_cache_bytes": "32768",
-        "cpu_l2_cache_bytes": "262144",
-        "cpu_l3_cache_bytes": "4194304",
-        "cpu_core_count": "2",
-        "cpu_frequency_max_hz": "3500000000",
-        "cpu_model_name": "Intel(R) Core(TM) i7-7567U CPU @ 3.50GHz",
-        "cpu_thread_count": "4",
-        "kernel_name": "19.6.0",
-        "memory_bytes": "17179869184",
-        "name": "diana",
-        "os_name": "macOS",
-        "os_version": "10.15.7",
-    },
-    "stats": {
-        "batch_id": "7b2fdd9f929d47b9960152090d47f8e6",
-        "run_id": "2a5709d179f349cba69ed242be3e6321",
-        "run_name": "commit: 02addad336ba19a654f9c857ede546331be7b631",
-        "data": [
-            "0.099094",
-            "0.037129",
-            "0.036381",
-            "0.148896",
-            "0.008104",
-            "0.005496",
-            "0.009871",
-            "0.006008",
-            "0.007978",
-            "0.004733",
-        ],
-        "times": [
-            "0.099094",
-            "0.037129",
-            "0.036381",
-            "0.148896",
-            "0.008104",
-            "0.005496",
-            "0.009871",
-            "0.006008",
-            "0.007978",
-            "0.004733",
-        ],
-        "unit": "s",
-        "time_unit": "s",
-        "iqr": "0.030442",
-        "iterations": 10,
-        "max": "0.148896",
-        "mean": "0.036369",
-        "median": "0.008988",
-        "min": "0.004733",
-        "q1": "0.006500",
-        "q3": "0.036942",
-        "stdev": "0.049194",
-        "timestamp": "2020-11-25T21:02:42.706806+00:00",
-    },
-    "tags": {
-        "compression": "snappy",
-        "cpu_count": 2,
-        "dataset": "nyctaxi_sample",
-        "file_type": "parquet",
-        "input_type": "arrow",
+        "name": "file-write",
     },
 }
 
 
 def test_upgrade():
-    VALID_PAYLOAD_1["tags"]["name"] = uuid.uuid4().hex
-    summary_1 = Summary.create(VALID_PAYLOAD_1)
-
-    VALID_PAYLOAD_2["tags"]["name"] = uuid.uuid4().hex
-    summary_2 = Summary.create(VALID_PAYLOAD_2)
+    VALID_PAYLOAD["tags"]["name"] = uuid.uuid4().hex
+    summary = Summary.create(VALID_PAYLOAD)
 
     # assert before migration
     distributions = Distribution.search(
-        filters=[Distribution.case_id == summary_1.case_id]
-    )
-    assert len(distributions) == 1
-    assert distributions[0].unit == "s"
-    assert distributions[0].observations == 1
-    assert distributions[0].mean_mean == decimal.Decimal("0.03636900000000000000")
-    assert distributions[0].mean_sd is None
-    assert distributions[0].min_mean == decimal.Decimal("0.00473300000000000000")
-    assert distributions[0].min_sd is None
-    assert distributions[0].max_mean == decimal.Decimal("0.14889600000000000000")
-    assert distributions[0].max_sd is None
-    assert distributions[0].median_mean == decimal.Decimal("0.00898800000000000000")
-    assert distributions[0].median_sd is None
-
-    distributions = Distribution.search(
-        filters=[Distribution.case_id == summary_2.case_id]
+        filters=[Distribution.case_id == summary.case_id]
     )
     assert len(distributions) == 1
     assert distributions[0].unit == "s"
@@ -220,22 +124,7 @@ def test_upgrade():
 
     # assert after migration
     distributions = Distribution.search(
-        filters=[Distribution.case_id == summary_1.case_id]
-    )
-    assert len(distributions) == 1
-    assert distributions[0].unit == "s"
-    assert distributions[0].observations == 1
-    assert distributions[0].mean_mean == decimal.Decimal("0.03636900000000000000")
-    assert distributions[0].mean_sd is None
-    assert distributions[0].min_mean == decimal.Decimal("0.00473300000000000000")
-    assert distributions[0].min_sd is None
-    assert distributions[0].max_mean == decimal.Decimal("0.14889600000000000000")
-    assert distributions[0].max_sd is None
-    assert distributions[0].median_mean == decimal.Decimal("0.00898800000000000000")
-    assert distributions[0].median_sd is None
-
-    distributions = Distribution.search(
-        filters=[Distribution.case_id == summary_2.case_id]
+        filters=[Distribution.case_id == summary.case_id]
     )
     assert len(distributions) == 1
     assert distributions[0].unit == "s"
--- migrations/versions/8ade76a4ebc0_backfill_distribution_2.py
+++ migrations/versions/8ade76a4ebc0_backfill_distribution_2.py
@@ -6,8 +6,8 @@ from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.sql import select
 
 
-revision = "0d4e564b1876"
-down_revision = "0d44e2332557"
+revision = "8ade76a4ebc0"
+down_revision = "0d4e564b1876"
 branch_labels = None
 depends_on = None
 
@@ -34,8 +34,6 @@ def get_distribution(
     commit_table,
     repository,
     sha,
-    case_id,
-    context_id,
     machine_hash,
     limit,
 ):
@@ -85,8 +83,6 @@ def get_distribution(
         .join(commits_up, commits_up.c.id == run_table.c.commit_id)
         .filter(
             run_table.c.name.like("commit: %"),
-            summary_table.c.case_id == case_id,
-            summary_table.c.context_id == context_id,
             func.concat(
                 machine_table.c.name,
                 "-",
@@ -112,45 +108,24 @@ def upgrade():
     run_table = meta.tables["run"]
     summary_table = meta.tables["summary"]
 
-    runs = connection.execute(run_table.select())
     commits = connection.execute(commit_table.select())
     distributions = connection.execute(distribution_table.select())
     machines = connection.execute(machine_table.select())
-    runs_by_id = {r["id"]: r for r in runs}
     commits_by_id = {c["id"]: c for c in commits}
     machines_by_id = {m["id"]: m for m in machines}
 
-    seen = {
-        f'{d["sha"]}{d["case_id"]}{d["context_id"]}{d["machine_hash"]}'
-        for d in distributions
-    }
-
-    summaries = connection.execute(
-        summary_table.select()
-        .join(run_table, run_table.c.id == summary_table.c.run_id)
-        .filter(run_table.c.name.like("commit: %"))
+    runs = connection.execute(
+        run_table.select().filter(run_table.c.name.like("commit: %"))
     )
-    for summary in summaries:
-        run = runs_by_id.get(summary["run_id"])
-        if not run:
-            continue
-
+    for run in runs:
         commit = commits_by_id.get(run["commit_id"])
         if not commit:
             continue
 
-        m = machines_by_id[summary["machine_id"]]
+        m = machines_by_id[run.machine_id]
         machine_hash = (
             f"{m.name}-{m.cpu_core_count}-{m.cpu_thread_count}-{m.memory_bytes}"
         )
-
-        hash_ = (
-            f'{commit["sha"]}{summary["case_id"]}{summary["context_id"]}{machine_hash}'
-        )
-        if hash_ in seen:
-            continue
-
-        seen.add(hash_)
 
         distributions = list(
             connection.execute(
@@ -161,32 +136,27 @@ def upgrade():
                     commit_table,
                     commit["repository"],
                     commit["sha"],
-                    summary["case_id"],
-                    summary["context_id"],
                     machine_hash,
                     1000,
                 )
             )
         )
 
-        if not distributions:
-            continue
+        for distribution in distributions:
+            values = dict(distribution)
+            machine_hash = values.pop("hash")
+            values["id"] = uuid.uuid4().hex
+            values["machine_hash"] = machine_hash
 
-        distribution = distributions[0]
-        values = dict(distribution)
-        machine_hash = values.pop("hash")
-        values["id"] = uuid.uuid4().hex
-        values["machine_hash"] = machine_hash
-
-        connection.execute(
-            insert(distribution_table)
-            .values(values)
-            .on_conflict_do_update(
-                index_elements=["sha", "case_id", "context_id", "machine_hash"],
-                set_=values,
+            connection.execute(
+                insert(distribution_table)
+                .values(values)
+                .on_conflict_do_update(
+                    index_elements=["sha", "case_id", "context_id", "machine_hash"],
+                    set_=values,
+                )
             )
-        )
-        connection.commit()
+            connection.commit()
 
 
 def downgrade():
