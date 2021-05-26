d6350f7c5c855cb57c104ef53ceb767580d5974f
Diana Clarke <diana.joan.clarke@gmail.com>
2021-05-26 17:38:27 -0600

    pytest -v

--- conbench/tests/api/test_compare.py
+++ conbench/tests/api/test_compare.py
@@ -108,11 +108,19 @@ class TestCompareBatchesGet(_asserts.GetEnforcer):
     url = "/api/compare/batches/{}/"
     public = True
 
-    def _create(self, with_ids=False, batch_id=None):
+    def _create(self, with_ids=False, run_id=None, batch_id=None):
         if batch_id is None:
             batch_id = uuid.uuid4().hex
-        summary1 = create_benchmark_summary("read", batch_id=batch_id)
-        summary2 = create_benchmark_summary("write", batch_id=batch_id)
+        summary1 = create_benchmark_summary(
+            "read",
+            run_id=run_id,
+            batch_id=batch_id,
+        )
+        summary2 = create_benchmark_summary(
+            "write",
+            run_id=run_id,
+            batch_id=batch_id,
+        )
         entity = FakeEntity(f"{batch_id}...{batch_id}")
         if with_ids:
             return [summary1.id, summary2.id], entity
@@ -121,16 +129,17 @@ class TestCompareBatchesGet(_asserts.GetEnforcer):
 
     def test_compare(self, client):
         self.authenticate(client)
-        batch_id = uuid.uuid4().hex
-        new_ids, compare = self._create(with_ids=True, batch_id=batch_id)
+        run_id, batch_id = uuid.uuid4().hex, uuid.uuid4().hex
+        new_ids, compare = self._create(
+            with_ids=True,
+            run_id=run_id,
+            batch_id=batch_id,
+        )
         response = client.get(f"/api/compare/batches/{compare.id}/")
 
         # cheating by comparing batch to same batch
         batch_ids = [batch_id, batch_id]
-        run_ids = [
-            "2a5709d179f349cba69ed242be3e6321",
-            "2a5709d179f349cba69ed242be3e6321",
-        ]
+        run_ids = [run_id, run_id]
         batches = ["read", "write"]
         benchmarks = [CASE, CASE]
         expected = _api_compare_list(
@@ -171,11 +180,19 @@ class TestCompareRunsGet(_asserts.GetEnforcer):
     url = "/api/compare/runs/{}/"
     public = True
 
-    def _create(self, with_ids=False, run_id=None):
+    def _create(self, with_ids=False, run_id=None, batch_id=None):
         if run_id is None:
             run_id = uuid.uuid4().hex
-        summary1 = create_benchmark_summary("read", run_id=run_id)
-        summary2 = create_benchmark_summary("write", run_id=run_id)
+        summary1 = create_benchmark_summary(
+            "read",
+            run_id=run_id,
+            batch_id=batch_id,
+        )
+        summary2 = create_benchmark_summary(
+            "write",
+            run_id=run_id,
+            batch_id=batch_id,
+        )
         entity = FakeEntity(f"{run_id}...{run_id}")
         if with_ids:
             return [summary1.id, summary2.id], entity
@@ -184,16 +201,17 @@ class TestCompareRunsGet(_asserts.GetEnforcer):
 
     def test_compare(self, client):
         self.authenticate(client)
-        run_id = uuid.uuid4().hex
-        new_ids, compare = self._create(with_ids=True, run_id=run_id)
+        run_id, batch_id = uuid.uuid4().hex, uuid.uuid4().hex
+        new_ids, compare = self._create(
+            with_ids=True,
+            run_id=run_id,
+            batch_id=batch_id,
+        )
         response = client.get(f"/api/compare/runs/{compare.id}/")
 
         # cheating by comparing run to same run
         run_ids = [run_id, run_id]
-        batch_ids = [
-            "7b2fdd9f929d47b9960152090d47f8e6",
-            "7b2fdd9f929d47b9960152090d47f8e6",
-        ]
+        batch_ids = [batch_id, batch_id]
         batches = ["read", "write"]
         benchmarks = [CASE, CASE]
         expected = _api_compare_list(
