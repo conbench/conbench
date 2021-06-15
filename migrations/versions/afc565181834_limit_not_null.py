703a6a00e2ea6d541dd8234c5250e62b14c03b96
Diana Clarke <diana.joan.clarke@gmail.com>
2021-06-15 14:14:20 -0600

    If NULL set to 1000

--- migrations/versions/afc565181834_limit_not_null.py
+++ migrations/versions/afc565181834_limit_not_null.py
@@ -1,5 +1,6 @@
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import MetaData
 
 
 revision = "afc565181834"
@@ -9,6 +10,17 @@ depends_on = None
 
 
 def upgrade():
+    connection = op.get_bind()
+    meta = MetaData()
+    meta.reflect(bind=connection)
+    distribution_table = meta.tables["distribution"]
+
+    connection.execute(
+        distribution_table.update()
+        .where(distribution_table.c.limit == None)  # noqa
+        .values(limit=1000)
+    )
+
     op.alter_column("distribution", "limit", existing_type=sa.INTEGER(), nullable=False)
 
 
