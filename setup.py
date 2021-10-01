a5e9af92c2966d19c04fe1dddc4e1a1e434e4544
Diana Clarke <diana.joan.clarke@gmail.com>
2021-10-01 13:27:43 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setuptools.setup(
     name="conbench",
-    version="1.23.0",
+    version="1.24.0",
     description="Continuous Benchmarking (CB) Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
