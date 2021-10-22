6238571ef0d9da05dbeaeed102262c86c22d7c04
Diana Clarke <diana.joan.clarke@gmail.com>
2021-10-22 16:08:00 -0600

    revert

--- conbench/tests/entities/test_user.py
+++ conbench/tests/entities/test_user.py
@@ -1,11 +1,11 @@
 from ...entities.user import User
 
 
-def foo_user_repr():
+def test_user_repr():
     user = User(name="Gwen Clarke", email="gwen@example.com")
     assert repr(user) == "<User gwen@example.com>"
 
 
-def foo_user_str():
+def test_user_str():
     user = User(name="Gwen Clarke", email="gwen@example.com")
     assert str(user) == "<User gwen@example.com>"
