c1465b3bb283469272a5c727030ad438cc97e8b5
Diana Clarke <diana.joan.clarke@gmail.com>
2021-11-13 10:24:59 -0700

    Fix recalculate distribution

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setuptools.setup(
     name="conbench",
-    version="1.49.0",
+    version="1.50.0",
     description="Continuous Benchmarking (CB) Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
