d6d61de5d5f3682a69075f24830ea14056bdc3c2
Elena Henderson <elena@e.local>
2022-01-11 15:00:57 -0800

    Fix test

--- conbench/tests/app/test_robots.py
+++ conbench/tests/app/test_robots.py
@@ -1,4 +1,4 @@
-def test_robots(self, client):
+def test_robots(client):
     response = client.get("/robots.txt")
     assert response.status_code == 200
     assert response.data == "User-Agent: *\nDisallow: /compare/\n"
