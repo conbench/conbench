29f395d3112bcad0fb5ea9a38e5cc980264f9aea
Elena Henderson <elena@e.local>
2022-03-03 12:46:13 -0800

    WIP

--- migrations/versions/e06cede49f65_add_error_column.py
+++ migrations/versions/e06cede49f65_add_error_column.py
@@ -16,6 +16,14 @@ branch_labels = None
 depends_on = None
 
 
+def set_run_has_errors_values():
+    connection = op.get_bind()
+    meta = sa.MetaData()
+    meta.reflect(bind=connection)
+    run_table = meta.tables["run"]
+    connection.execute(run_table.update().values(has_errors=False))
+
+
 def upgrade():
     op.add_column(
         "summary",
@@ -28,9 +36,13 @@ def upgrade():
     op.alter_column("summary", "time_unit", existing_type=sa.TEXT(), nullable=True)
     op.alter_column("summary", "batch_id", existing_type=sa.TEXT(), nullable=True)
     op.alter_column("summary", "iterations", existing_type=sa.INTEGER(), nullable=True)
+    op.add_column("run", sa.Column("has_errors", sa.Boolean()))
+    set_run_has_errors_values()
+    op.alter_column("run", "has_errors", existing_type=sa.Boolean, nullable=False)
 
 
 def downgrade():
+    op.drop_column("run", "has_errors")
     op.alter_column("summary", "iterations", existing_type=sa.INTEGER(), nullable=False)
     op.alter_column("summary", "batch_id", existing_type=sa.TEXT(), nullable=False)
     op.alter_column("summary", "time_unit", existing_type=sa.TEXT(), nullable=False)
