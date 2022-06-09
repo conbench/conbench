97c4882e08183687a930af45f695b3ac7d63b58b
Austin Dickey <austin3spammy@gmail.com>
2022-06-09 14:32:50 -0500

    remove backfill code

--- migrations/versions/bc0ac747cec6_add_run_reason_column.py
+++ migrations/versions/bc0ac747cec6_add_run_reason_column.py
@@ -18,15 +18,6 @@ depends_on = None
 def upgrade():
     op.add_column("run", sa.Column("reason", sa.String(length=250), nullable=True))
 
-    # use some custom logic to backfill the column
-    op.execute("UPDATE run SET reason = 'commit' WHERE name LIKE 'commit%'")
-    op.execute("UPDATE run SET reason = 'pull request' WHERE name LIKE 'pull request%'")
-    op.execute(
-        "UPDATE run SET reason = 'manual commit' WHERE name LIKE 'manual commit%'"
-    )
-    op.execute("UPDATE run SET reason = 'merge commit' WHERE name LIKE '%merge-commit'")
-    op.execute("UPDATE run SET reason = 'nightly' WHERE name LIKE '%nightly'")
-
 
 def downgrade():
     op.drop_column("run", "reason")
