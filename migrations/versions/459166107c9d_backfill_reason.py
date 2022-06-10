5b127c61528e0fd3f67731013c88ffac46b95b56
Austin Dickey <austin3spammy@gmail.com>
2022-06-10 16:22:34 -0500

    Backfilled rows with empty reasons (#356)
    * Backfilled rows with empty reasons
    
    * appease the linter

---
+++ migrations/versions/459166107c9d_backfill_reason.py
@@ -0,0 +1,51 @@
+"""backfill reason
+
+Revision ID: 459166107c9d
+Revises: bc0ac747cec6
+Create Date: 2022-06-10 09:49:05.883111
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "459166107c9d"
+down_revision = "bc0ac747cec6"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+        UPDATE run SET reason = 'nightly'
+        WHERE reason IS NULL AND name LIKE '%nightly';
+        """
+    )
+    op.execute(
+        """
+        UPDATE run SET reason = 'commit'
+        WHERE reason IS NULL AND name LIKE 'commit%';
+        """
+    )
+    op.execute(
+        """
+        UPDATE run SET reason = 'pull request'
+        WHERE reason IS NULL AND name LIKE 'pull request%';
+        """
+    )
+    op.execute(
+        """
+        UPDATE run SET reason = 'manual'
+        WHERE reason IS NULL AND name LIKE 'manual%';
+        """
+    )
+    op.execute(
+        """
+        UPDATE run SET reason = 'manual'
+        WHERE reason IS NULL AND name LIKE 'test%';
+        """
+    )
+
+
+def downgrade():
+    pass
