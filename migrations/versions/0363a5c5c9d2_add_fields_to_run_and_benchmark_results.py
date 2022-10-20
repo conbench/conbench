c7d7ab397d57913c9763fc8a1085fc17d98e5aec
Elena Henderson <elena@e.local>
2022-10-20 11:19:17 -0700

    Added migrations

---
+++ migrations/versions/0363a5c5c9d2_add_fields_to_run_and_benchmark_results.py
@@ -0,0 +1,47 @@
+"""add_fields_to_run_and_benchmark_results
+
+Revision ID: 0363a5c5c9d2
+Revises: 459166107c9d
+Create Date: 2022-10-20 11:14:01.529869
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = "0363a5c5c9d2"
+down_revision = "459166107c9d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "benchmark_result",
+        sa.Column("validation", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+    )
+    op.add_column(
+        "run", sa.Column("info", postgresql.JSONB(astext_type=sa.Text()), nullable=True)
+    )
+    op.add_column(
+        "run",
+        sa.Column("error_info", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+    )
+    op.add_column("run", sa.Column("error_type", sa.String(length=250), nullable=True))
+    op.add_column(
+        "run",
+        sa.Column(
+            "started_at", sa.DateTime(), server_default=sa.text("now()"), nullable=False
+        ),
+    )
+    op.add_column("run", sa.Column("finished_at", sa.DateTime(), nullable=True))
+
+
+def downgrade():
+    op.drop_column("run", "finished_at")
+    op.drop_column("run", "started_at")
+    op.drop_column("run", "error_type")
+    op.drop_column("run", "error_info")
+    op.drop_column("run", "info")
+    op.drop_column("benchmark_result", "validation")
