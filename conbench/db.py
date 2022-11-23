1a25772792248318d8a48de48730e35d3490986a
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2022-11-23 14:07:28 +0100

    db create_all: apply retrying strategy for test env

--- conbench/db.py
+++ conbench/db.py
@@ -1,9 +1,19 @@
+import logging
+
+
+import sqlalchemy.exc
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+import tenacity
+
 
 engine = None
 session_maker = sessionmaker(future=True)
 Session = scoped_session(session_maker)
+
+
+log = logging.getLogger(__name__)
 
 
 def configure_engine(url):
@@ -19,6 +29,27 @@ def configure_engine(url):
     session_maker.configure(bind=engine)
 
 
+def log_after_retry_attempt(retry_state: tenacity.RetryCallState):
+    log.info(
+        "result after attempt %s for %s: %s",
+        retry_state.attempt_number,
+        str(retry_state.fn),
+        str(retry_state.outcome.exception()),
+    )
+
+
+# `create_all()` below can fail with an `OperationalError` when the database
+# isn't yet reachable. Can happen when web app and database are launched at
+# about the same time (likely to happen only in dev environment). Apply a
+# retrying strategy.
+@tenacity.retry(
+    retry=tenacity.retry_if_exception_type(sqlalchemy.exc.OperationalError),
+    stop=tenacity.stop_after_attempt(10),
+    wait=tenacity.wait_fixed(1),
+    before=tenacity.before_log(log, logging.DEBUG),
+    after=log_after_retry_attempt,
+    reraise=True,
+)
 def create_all():
     from .entities._entity import Base
 
