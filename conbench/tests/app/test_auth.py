12ff5f569f5aa337da559db3febe5bae3e68a6f8
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2022-11-23 15:47:23 +0100

    tests: cover oidc flow against dex

--- conbench/tests/app/test_auth.py
+++ conbench/tests/app/test_auth.py
@@ -1,5 +1,13 @@
+import logging
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
 from ...config import TestConfig
 from ...tests.app import _asserts
+
+log = logging.getLogger(__name__)
 
 
 class TestRegister(_asserts.AppEndpointTest):
@@ -129,3 +137,124 @@ class TestLogout(_asserts.AppEndpointTest):
         # cannot get users page after
         response = client.get("/users/", follow_redirects=True)
         self.assert_login_page(response)
+
+
+class TestLoginOIDC(_asserts.AppEndpointTest):
+    def test_login_page_shows_sso_link(self, client):
+        r = client.get("/login/", follow_redirects=True)
+        assert "Google Login" in r.text
+
+    def test_oidc_flow_against_dex(self, client):
+
+        # r = client.get("/login/", follow_redirects=True)
+        # TODO: parse this from the HTML doc
+        # initiate_flow_url = "http://127.0.0.1:5000/api/google/"
+        # follow_redirects=True)
+        r0 = client.get("/api/google/")
+
+        # `r0` is meant to be a redirect response, redirecting to the identity
+        # provider. Extract the full URL we've been redirected to. The URL
+        # represents a so-called authorization request, with all the parameters
+        # for that request being encoded in the URL query parameters
+        authorization_request_url = r0.headers["location"]
+        log.info(f"{authorization_request_url=}")
+
+        # Emit authorization request against identity provider.
+        r1 = requests.get(authorization_request_url)
+
+        # We are not yet logged in to the identity provider which is why
+        # we expect it to send a login page in the response. That login
+        # page shows an email/password login form. Extract form submission
+        # endpoint URL from login page. It's a relative URL.
+        assert r1.status_code == 200
+        rel_login_post_url = parse_login_page(r1.text)
+        log.info("rel_login_post_url: %s", rel_login_post_url)
+
+        # Construct absolute form submission endpoing URL. urljoin() will
+        # pick only scheme and DNS name from the authorization_request_url
+        # because the rel_login_post_url is expected to start with a slash.
+        login_post_url = urljoin(authorization_request_url, rel_login_post_url)
+
+        # Defined in Dex' static configuration document.
+        login_form_data = {
+            "login": "admin@example.com",
+            "password": "password",
+        }
+
+        log.info("login_post_url: %s", login_post_url)
+        log.info("login_form_data: %s", login_form_data)
+        r2 = requests.post(login_post_url, data=login_form_data)
+        log.info("response to POSTing credentials: %s", r2.status_code)
+
+        # When bad credentials are provided the response status code is
+        # still 200, with this text in the HTML doc:
+        assert "Invalid Email Address and password" not in r2.text
+
+        # `r2.text` is expected to be an HTML document containing the
+        # consent form. Parse the document and POST the form data to the
+        # same endpoint that served it.
+        consent_form_data = parse_consent_review_page(r2.text)
+
+        # Extract the full URL for the endpoint that served the consent
+        # form from the previous response.
+        consent_post_url = r2.url
+
+        log.info("consent_post_url: %s", consent_post_url)
+        log.info("consent_form_data: %s", consent_form_data)
+        log.info("post consent form")
+        r3 = requests.post(
+            consent_post_url, data=consent_form_data, allow_redirects=False
+        )
+
+        # Upon success, the response emitted by Dex redirects back to the
+        # relying party (Conbench) which (in the back-channel) communicates
+        # with the OP (Dex) and eventually emits the authentication proof
+        assert str(r3.status_code).startswith("3")
+        callback_request_url = r3.headers["location"]
+        log.info(f"{callback_request_url=}")
+
+        # In this test suite the web application is not exposed by a real HTTP
+        # server. It can only be reached with the test client `client` -- which
+        # seems to deal fine with the absolute nature of
+        # `callback_request_url`.
+        r4 = client.get(callback_request_url)
+        log.info(r4.headers)
+
+        # Confirm that the api has returned authentication proof.
+        assert "set-cookie" in r4.headers
+        assert "session" in r4.headers["set-cookie"]
+
+
+def parse_login_page(html):
+    """Parse HTML, extract all info required for form submission.
+    If the `connectors` enumeration in Dex' config.yaml is empty, then this
+    landing page presents a form. If it is not empty, then this landing page
+    requires selecting one of the connectors. Rely on no connectors being
+    defined.
+    """
+    page = BeautifulSoup(html, features="lxml")
+
+    # Dex emits just a relative POST URL in the login HTML doc.
+    rel_post_url = page.find("form")["action"]
+
+    return rel_post_url
+
+
+def parse_consent_review_page(html):
+    """Parse HTML, extract all info required for form submission.
+    Form example:
+            <form method="post">
+            <input type="hidden" name="req" value="vinfxhadooorlhxikzae"/>
+            <input type="hidden" name="approval" value="approve">
+            <button type="submit" class="dex-btn theme-btn--success">
+                <span class="dex-btn-text">Grant Access</span>
+            </button>
+            </form>
+    """
+    page = BeautifulSoup(html, features="lxml")
+    form_data = {
+        "approval": "approve",
+        "req": page.find("input", attrs={"name": "req"})["value"],
+    }
+
+    return form_data
