3ee3265383a3c7f33a1edb53ebe2e17d4c6a0f64
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2022-11-24 17:48:22 +0100

    logger.setup(): fix invocation

--- conbench/__init__.py
+++ conbench/__init__.py
@@ -14,7 +14,7 @@ del importlib_metadata
 
 
 # Pre-configure logging before reading user-given configuration.
-conbench.logger.setup(level_stderr="DEBUG", level_file=None)
+conbench.logger.setup(level_stderr="DEBUG", level_file=None, level_sqlalchemy="WARNING")
 log = logging.getLogger(__name__)
 
 
