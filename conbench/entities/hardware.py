12f4f76828c530d20f3e31f900f7a8959cd19b33
Austin Dickey <austin3spammy@gmail.com>
2022-11-30 13:50:49 -0600

    make a temp change, expected to fail

--- conbench/entities/hardware.py
+++ conbench/entities/hardware.py
@@ -190,7 +190,13 @@ class MachineCreate(marshmallow.Schema):
 class ClusterCreate(marshmallow.Schema):
     name = marshmallow.fields.String(required=True)
     info = marshmallow.fields.Dict(required=True)
-    optional_info = marshmallow.fields.Dict(required=True)
+    optional_info = marshmallow.fields.Dict(
+        required=True,
+        metadata={
+            "description": "Additional optional information about the cluster, which is not likely to impact the "
+            "benchmark performance (e.g. region, settings like logging type, etc)."
+        },
+    )
 
 
 class MachineSchema:
