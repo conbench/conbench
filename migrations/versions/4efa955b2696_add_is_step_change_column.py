b18c1d82d665607075deac15e2c7923f158380b1
Austin Dickey <austin3spammy@gmail.com>
2023-01-05 10:04:43 -0600

    fix migration revises

--- migrations/versions/4efa955b2696_add_is_step_change_column.py
+++ migrations/versions/4efa955b2696_add_is_step_change_column.py
@@ -1,7 +1,7 @@
 """add is_step_change column
 
 Revision ID: 4efa955b2696
-Revises: d3515ecea53d
+Revises: 4d8d67396f79
 Create Date: 2022-12-06 12:50:12.671498
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "4efa955b2696"
-down_revision = "d3515ecea53d"
+down_revision = "4d8d67396f79"
 branch_labels = None
 depends_on = None
 
