fed3d11910280e4bda80d2e0274cdf239f0d0f09
Austin Dickey <austin@voltrondata.com>
2023-01-10 11:53:22 -0600

    Fix step change migration and branch limit (#585)
    * Fix step change migration and branch limit
    
    * lint

--- migrations/env.py
+++ migrations/env.py
@@ -66,6 +66,7 @@ def run_migrations_offline():
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_type=True,
     )
 
     with context.begin_transaction():
@@ -87,7 +88,9 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata, compare_type=True
+        )
 
         with context.begin_transaction():
             context.run_migrations()
---
+++ migrations/versions/498ade3999dd_add_is_step_change_fix_branch_limit.py
@@ -0,0 +1,41 @@
+"""add is_step_change, fix branch limit
+
+Revision ID: 498ade3999dd
+Revises: 4d8d67396f79
+Create Date: 2023-01-10 11:13:40.647075
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "498ade3999dd"
+down_revision = "4d8d67396f79"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # This mistake needs to be corrected
+    op.alter_column(
+        "commit",
+        "branch",
+        existing_type=sa.VARCHAR(length=100),
+        type_=sa.String(length=510),
+    )
+    # First allow this to be nullable, then fill it in and make it non-nullable
+    op.add_column(
+        "benchmark_result", sa.Column("is_step_change", sa.Boolean(), nullable=True)
+    )
+    op.execute("UPDATE benchmark_result SET is_step_change = false")
+    op.alter_column("benchmark_result", "is_step_change", nullable=False)
+
+
+def downgrade():
+    op.alter_column(
+        "commit",
+        "branch",
+        existing_type=sa.String(length=510),
+        type_=sa.VARCHAR(length=100),
+    )
+    op.drop_column("benchmark_result", "is_step_change")
