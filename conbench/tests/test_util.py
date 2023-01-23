bfbf6d8ab178d85ac940beff3b27b34a1892956f
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-01-23 15:17:55 +0100

    pytest parameterize

--- conbench/tests/test_util.py
+++ conbench/tests/test_util.py
@@ -1,26 +1,21 @@
 from datetime import timezone
 
+import pytest
+
 from conbench.util import tznaive_iso8601_to_tzaware_dt
 
 
-def test_tznaive_iso8601_to_tzaware_dt():
-    # Confirm that UTC tzinfo is attached.
-    dt = tznaive_iso8601_to_tzaware_dt("2022-03-03T19:48:06")
-    assert dt.tzinfo == timezone.utc
-    assert dt.year == 2022
-    assert dt.second == 6
-    assert dt.hour == 19
-
-    # Confirm that UTC tzinfo in input is OK.
-    dt = tznaive_iso8601_to_tzaware_dt("2022-03-03T19:48:06Z")
-    assert dt.tzinfo == timezone.utc
-    assert dt.year == 2022
-    assert dt.second == 6
-    assert dt.hour == 19
-
-    # Confirm that any non-UTC tzinfo in the input gets replaced with
-    # UTC, retaining the numbers.
-    dt = tznaive_iso8601_to_tzaware_dt("2022-03-03T19:48:06+02:00")
+@pytest.mark.parametrize(
+    "teststring",
+    ["2022-03-03T19:48:06", "2022-03-03T19:48:06Z", "2022-03-03T19:48:06+02:00"],
+)
+def test_tznaive_iso8601_to_tzaware_dt(teststring: str):
+    # Confirm that
+    # - UTC tzinfo is attached.
+    # - UTC tzinfo in input is OK.
+    # - any non-UTC tzinfo in the input gets replaced with
+    #   UTC, retaining the numbers.
+    dt = tznaive_iso8601_to_tzaware_dt(teststring)
     assert dt.tzinfo == timezone.utc
     assert dt.year == 2022
     assert dt.second == 6
