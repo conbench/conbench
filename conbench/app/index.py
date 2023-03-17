2bf7c1225349c1434e38a569c7b18f2ea3f7ab2e
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-03-17 11:18:15 +0100

    landing page: re-structure query
    - do not go through API layer anymore (save one
      re-serialization roundtrip). This is for #890.
    
    - this is not a functional commit
    
    - use new query technique, has great typing result

--- conbench/app/index.py
+++ conbench/app/index.py
@@ -1,9 +1,21 @@
+from collections import defaultdict
+
+
+from dataclasses import dataclass
+
+
+from typing import Optional
+
 import flask as f
 
+from sqlalchemy import select
+
+from ..db import Session
 from ..app import rule
 from ..app._endpoint import AppEndpoint, authorize_or_terminate
 from ..app.benchmarks import RunMixin
 from ..config import Config
+from ..entities.run import Run
 
 
 class Index(AppEndpoint, RunMixin):
@@ -18,8 +30,14 @@ class Index(AppEndpoint, RunMixin):
 
     @authorize_or_terminate
     def get(self):
-        runs = self.get_display_runs()
         return self.page(runs)
+        # Following
+        # https://docs.sqlalchemy.org/en/20/orm/queryguide/select.html#selecting-orm-entities
+        runs = Session.scalars(
+            select(Run).order_by(Run.timestamp.desc()).limit(1000)
+        ).all()
+
+
 
 
 view = Index.as_view("index")
