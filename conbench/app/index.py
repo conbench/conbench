8ca08d688547c71ee02aa5ff4e605ec542818663
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-03-17 11:21:35 +0100

    landing page: group runs by repo

--- conbench/app/index.py
+++ conbench/app/index.py
@@ -19,25 +19,65 @@ from ..entities.run import Run
 
 
 class Index(AppEndpoint, RunMixin):
-    def page(self, runs):
+    def page(self, repo_runs_map):
         return self.render_template(
             "index.html",
             application=Config.APPLICATION_NAME,
             title="Home",
-            runs=runs,
-            search_value=f.request.args.get("search"),
+            repo_runs_map=repo_runs_map,
+            # Note(JP): search_value is not consumed in template
+            # search_value=f.request.args.get("search"),
         )
 
     @authorize_or_terminate
     def get(self):
-        return self.page(runs)
         # Following
         # https://docs.sqlalchemy.org/en/20/orm/queryguide/select.html#selecting-orm-entities
         runs = Session.scalars(
             select(Run).order_by(Run.timestamp.desc()).limit(1000)
         ).all()
 
+        # Note(JP): group runs by associated commit.repository value Note that
+        # consistency between benchmark results in the run is currently not
+        # granted: https://github.com/conbench/conbench/issues/864
 
+        repo_runs_map = defaultdict(list)
+
+        for r in runs:
+            rd = RunForDisplay(
+                ctime_for_table=r.timestamp.strftime("%Y-%m-%d %H:%M:%S UTC"),
+                commit_message_short=short_commit_msg(r.commit.message),
+                run=r,
+            )
+            repo_runs_map[r.commit.repository].append(rd)
+
+        return self.page(repo_runs_map)
+
+
+@dataclass
+class RunForDisplay:
+    ctime_for_table: str
+    commit_message_short: str
+    # Also expose the raw Run object
+    run: Run
+    # commit_url: Optional[str]
+
+
+def short_commit_msg(msg: str):
+    """
+    Substitute multiple whitespace characters with a single space. Overall,
+    truncate at maxlen.
+
+    This may return an empty string.
+    """
+    result = " ".join(msg.split())
+
+    maxlen = 200
+
+    if len(result) > maxlen:
+        result = result[:maxlen] + "..."
+
+    return result
 
 
 view = Index.as_view("index")
