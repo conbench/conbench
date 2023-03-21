3e270dd60ca2466ccf2d34b17883efdabe7fa7d9
Dr. Jan-Philip Gehrcke <jp@voltrondata.com>
2023-03-21 16:32:32 +0100

    db: define connect timeout (#920)
    * db: define connect timeout
    
    Related to #801.
    
    For example, allows for quicker retrying upon
    networking hiccup in the context of docker-compose.
    
    * db.py: better explain connect_timeout arg

--- conbench/db.py
+++ conbench/db.py
@@ -38,7 +38,13 @@ def configure_engine(url):
         # tickets and discussions:
         # https://github.com/conbench/conbench/issues/599
         # https://github.com/conbench/conbench/pull/690
-        connect_args={"options": "-c timezone=utc -c statement_timeout=60s"},
+        # https://docs.sqlalchemy.org/en/20/core/engines.html#use-the-connect-args-dictionary-parameter
+        connect_args={
+            "options": "-c timezone=utc -c statement_timeout=60s",
+            # The `connect_timeout` parameter is documented in
+            # https://www.postgresql.org/docs/12/libpq-connect.html
+            "connect_timeout": 3,  # unit: seconds
+        },
     )
     log.info("bind engine to session")
     session_maker.configure(bind=engine)
@@ -132,6 +138,7 @@ def create_all():
         else:
             raise
 
+    log.info("create_all(engine) returned. dispose()")
     engine.dispose()
 
 
