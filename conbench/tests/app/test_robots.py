873f98fd87013bee5761a69ab9d46d2a29f2940c
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-04-02 21:34:44 +0200

    add test for user agent deny list

--- conbench/tests/app/test_robots.py
+++ conbench/tests/app/test_robots.py
@@ -8,3 +8,7 @@ def test_robots(client):
     response = client.get("/robots.txt")
     assert response.status_code == 200
     assert response.get_data(as_text=True) == expected_text
+def test_user_agent_denylist(client):
+    for path in ("/api/ping/", "/", "/api/runs/"):
+        resp = client.get(path)
+        assert resp.status_code == 403
