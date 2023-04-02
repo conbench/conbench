c2590153f032325eabbb66dda6dae95b1e5c5987
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-04-02 21:35:09 +0200

    tests: add comment

--- conbench/tests/test_version.py
+++ conbench/tests/test_version.py
@@ -5,5 +5,7 @@ import conbench
 __version__ = importlib_metadata.version("conbench")
 
 
+# Note(JP): it's a little tough to understand what this test is testing.
+# Maybe we should test if the /api/ping endpoint emits the expected version.
 def test_version():
     assert __version__ == conbench.__version__
