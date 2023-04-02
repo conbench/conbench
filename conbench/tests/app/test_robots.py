d5c1cf50dbb8e26bf467854060e3dbd5f36246c7
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-04-02 21:35:01 +0200

    robots.txt: discourage scraping

--- conbench/tests/app/test_robots.py
+++ conbench/tests/app/test_robots.py
@@ -1,13 +1,9 @@
-expected_text = """
-User-Agent: *
-Allow: /
-"""
-
-
-def test_robots(client):
+def test_robots_txt(client):
     response = client.get("/robots.txt")
     assert response.status_code == 200
-    assert response.get_data(as_text=True) == expected_text
+    assert response.text == "User-Agent: *\nDisallow: /"
+
+
 def test_user_agent_denylist(client):
     for path in ("/api/ping/", "/", "/api/runs/"):
         resp = client.get(path)
