d23c075aec08051c95a8e5cc0d2e047c22d8b4c8
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-04-06 11:41:49 +0200

    app: gunicorn: central cfg, single-process N threads
    See issue #1018.

--- conbench/gunicorn-conf.py
+++ conbench/gunicorn-conf.py
@@ -43,3 +43,43 @@ from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMet
 
 def child_exit(server, worker):
     GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
+
+
+# Canonical gunicorn config below this line.
+# https://docs.gunicorn.org/en/stable/settings.html#config-file
+
+
+bind = ["0.0.0.0:5000"]
+
+wsgi_app = "conbench:application"
+
+# Canonical format, plus response generation duration in seconds.
+access_log_format = (
+    '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" (%(L)s s)'
+)
+
+errorlog = "-"  # emit to stderr
+accesslog = "-"  # emit to stdout
+loglevel = "info"  # d efault
+
+# requires setprocname
+# proc_name = 'conbench-gunicorn'
+
+# Reduce connection backlog from default (2048)
+backlog = 300
+
+# Run gunicorn as a single-process N thread model (these are real threads,
+# based on CPython threading.Thread, using Unix pthreads). Assume that C copies
+# of this are created by higher-level orchestration (so that more than one CPU
+# core is after all serving requests).
+# https://github.com/conbench/conbench/issues/1018
+workers = 1
+threads = 10
+
+# This is the worker timeout; an observer process will terminate the observed
+# worker process if the observed process hasn't responded within that
+# timeframe. This was more relevant at times when we ran more than one worker
+# process per gunicorn, and a single HTTP request could render a single process
+# occupied. Keep a large value for now, for the case where all threads in the
+# process process genuine requests (which all take a while to respond to)
+timeout = 120
