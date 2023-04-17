f77194ceb3d9f2ec156db60d5d03a7847ef1e9e5
Dr. Jan-Philip Gehrcke <jp@voltrondata.com>
2023-04-17 14:43:30 +0200

    _entity.py: change query idiom (use type info), introduce robust get_or_create() functions (#1125)
    * _entity.py: new query idiom, returns type info
    
    * benchmark_result.py: more robust get_or_create()
    
    for info, case, context
    
    * add type annotation
    
    * rollback after integrityerror
    
    * lint

--- conbench/entities/info.py
+++ conbench/entities/info.py
@@ -3,6 +3,8 @@ import sqlalchemy as s
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import Mapped
 
+from conbench.db import Session
+
 from ..entities._entity import (
     Base,
     EntityMixin,
@@ -16,6 +18,26 @@ class Info(Base, EntityMixin):
     __tablename__ = "info"
     id: Mapped[str] = NotNull(s.String(50), primary_key=True, default=generate_uuid)
     tags: Mapped[dict] = NotNull(postgresql.JSONB)
+
+
+def get_info_or_create(info_dict) -> Info:
+    """
+    Try to create, but expect conflict (work with unique constraint on
+    name/tags).
+
+    Return (newly created, or previously existing) object, or raise an
+    exception.
+    """
+    try:
+        return Info.create(info_dict)
+    except s.exc.IntegrityError as exc:
+        if "violates unique constraint" not in str(exc):
+            raise
+
+    Session.rollback()
+    i = Info.first(**info_dict)
+    assert i is not None
+    return i
 
 
 s.Index("info_index", Info.tags, unique=True)
