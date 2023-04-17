efd8a594b661298144f0e956258c485c43e7ac77
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-04-17 18:57:28 +0200

    add comment

--- conbench/tests/api/_fixtures.py
+++ conbench/tests/api/_fixtures.py
@@ -128,6 +128,9 @@ VALID_RESULT_PAYLOAD_WITH_ERROR = dict(
 
 VALID_RESULT_PAYLOAD_WITH_ITERATION_ERROR = dict(
     run_id="ya5709d179f349cba69ed242be3e6323",
+    # It's interesting that this has both, the `error` property set and
+    # missing iteration data. Either is sufficient for marking a result as
+    # "errored".
     error={"stack_trace": "some trace", "command": "ls"},
     **{
         key: value
