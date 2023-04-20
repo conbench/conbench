3cbe3027e4de883865f3acae02ba0479337a9df9
Dr. Jan-Philip Gehrcke <jp@voltrondata.com>
2023-04-20 18:14:25 +0200

    _entity.py: add type-generic get_or_create() method (#1144)
    * _entity.py: add type-generic get_or_create() method
    
    * webapp: use new get_or_create()

--- conbench/entities/context.py
+++ conbench/entities/context.py
@@ -3,37 +3,13 @@ import sqlalchemy as s
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import Mapped
 
-from conbench.db import Session
-
 from ..entities._entity import Base, EntityMixin, EntitySerializer, NotNull, genprimkey
 
 
-# Maybe EntityMixin should be a generic class
-# https://mypy.readthedocs.io/en/latest/generics.html#defining-subclasses-of-generic-classes
-class Context(Base, EntityMixin):
+class Context(Base, EntityMixin["Context"]):
     __tablename__ = "context"
     id: Mapped[str] = NotNull(s.String(50), primary_key=True, default=genprimkey)
     tags: Mapped[dict] = NotNull(postgresql.JSONB)
-
-
-def get_context_or_create(ctx_dict) -> Context:
-    """
-    Try to create, but expect conflict (work with unique constraint on
-    name/tags).
-
-    Return (newly created, or previously existing) object, or raise an
-    exception.
-    """
-    try:
-        return Context.create(ctx_dict)
-    except s.exc.IntegrityError as exc:
-        if "violates unique constraint" not in str(exc):
-            raise
-
-    Session.rollback()
-    c = Context.first(**ctx_dict)
-    assert c is not None
-    return c
 
 
 s.Index("context_index", Context.tags, unique=True)
