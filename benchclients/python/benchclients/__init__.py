bc8b99059d95c5ab13240e168803c4313477b456
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-04-21 15:17:02 +0200

    adjust benchclients init

--- benchclients/python/benchclients/__init__.py
+++ benchclients/python/benchclients/__init__.py
@@ -1,6 +1,4 @@
 from ._version import __version__
-from .base import BaseClient
 from .conbench import ConbenchClient
-from .logging import fatal_and_log, log
 
-__all__ = ["__version__", "BaseClient", "ConbenchClient", "fatal_and_log", "log"]
+__all__ = ["__version__", "ConbenchClient"]
