70cb5c0d663a625e3d1966079135d7a5f55cccf0
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-04-21 17:11:08 +0200

    re-add

--- benchclients/python/benchclients/__init__.py
+++ benchclients/python/benchclients/__init__.py
@@ -1,4 +1,6 @@
 from ._version import __version__
+from .base import BaseClient
 from .conbench import ConbenchClient
+from .logging import fatal_and_log, log
 
-__all__ = ["__version__", "ConbenchClient"]
+__all__ = ["__version__", "BaseClient", "ConbenchClient", "fatal_and_log", "log"]
