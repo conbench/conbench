b2966f66455519fbdaca892aeadfc9895d66ee02
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-05-09 21:56:03 +0200

    bmrt cache: store case dictionary

--- conbench/entities/case.py
+++ conbench/entities/case.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import sqlalchemy as s
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import Mapped
@@ -11,6 +13,16 @@ class Case(Base, EntityMixin["Case"]):
     # The name of the conceptual benchmark (store on BenchmarkResult directly)?
     name: Mapped[str] = NotNull(s.Text)
     tags: Mapped[dict] = NotNull(postgresql.JSONB)
+
+    def to_dict(self) -> Dict:
+        """
+        Be sure to return `self.tags` directly so that we do not need to keep a
+        huge amount of copies of this in memory (this may be shared across many
+        benchmark result objects). This here is basically just an indirection
+        from the name "tags" to "to_dict()" which I think is more intuitive to
+        work with. Think "case dictionary".
+        """
+        return self.tags
 
 
 s.Index("case_index", Case.name, Case.tags, unique=True)
