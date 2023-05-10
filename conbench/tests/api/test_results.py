f2399955e6d8ef228eb8612f43c63ab397fbab67
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-05-10 14:27:23 +0200

    test_results: add test for run_reason arg

--- conbench/tests/api/test_results.py
+++ conbench/tests/api/test_results.py
@@ -344,6 +344,13 @@ class TestBenchmarkList(_asserts.ListEnforcer):
         response = client.get("/api/benchmarks/?run_id=200")
         self.assert_200_ok(response, [_expected_entity(benchmark_result)])
 
+    def test_benchmark_list_filter_by_run_reason(self, client):
+        self.authenticate(client)
+        _fixtures.benchmark_result(run_id="100", reason="rolf")
+        benchmark_result = _fixtures.benchmark_result(run_id="100")
+        response = client.get("/api/benchmarks/?run_reason=rolf")
+        self.assert_200_ok(response, [_expected_entity(benchmark_result)])
+
     def test_benchmark_list_filter_by_multiple_run_id(self, client):
         self.authenticate(client)
         benchmark_result_1 = _fixtures.benchmark_result()
