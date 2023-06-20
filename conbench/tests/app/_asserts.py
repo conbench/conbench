d97d28fd8b0fc8d2b3df126042e43b3c1dadf39e
Dr. Jan-Philip Gehrcke <jp@voltrondata.com>
2023-06-20 12:44:20 +0200

    UI: ci run view: clean up (#1327)
    * runs templ: remove breadcrumb
    
    * run view: never show history plot, outliers
    
    This hasn't been used really. It is also
    hard to maintain (goals unclear).
    
    Can certainly re-introduce this when desired
    and when better defined.
    
    For now, I look at this view as "display
    information about a single run".
    
    With links to more interesting analyses.
    No plots on this page.
    Yes: show benchmark results in this run,
    and metadata about run.
    
    * run view: use heading, tweak viz
    
    * runs.py: try to clean up a bit
    
    * run.html: do not pass run_id twice
    
    * remove outcommented code
    
    * run.html: a little more spacing
    
    * lint
    
    * fix table, adjust js, remove dead code
    
    * error_page templ: tweak
    
    * tweak
    
    * fix tests
    
    do not mix error page and actual page.
    
    * fix test

--- conbench/tests/app/_asserts.py
+++ conbench/tests/app/_asserts.py
@@ -214,9 +214,16 @@ class GetEnforcer(Enforcer):
         # match a certain pattern. We can make this nicer via UI later,
         # but for starters a non-matching URL pattern is OK / nice to be
         # treated as 404 not found (with a hint for the user).
-        if "compare" in unknown_url and response.status_code == 404:
-            if "not found: ellipsis (...) expected as part of URL":
-                return
+        if "compare" in unknown_url:  # and response.status_code == 404:
+            assert "Got unexpected URL path pattern. Expected" in response.text
+            return
+            # if "not found: ellipsis (...) expected as part of URL" in response.text:
+            #     return
+
+        # Common code... not always good.
+        if "runs/unknown" in unknown_url:
+            assert "Run ID unknown: unknown" in response.text
+            return
 
         if getattr(self, "redirect_on_unknown", True):
             assert b"local-dev-conbench - Home" in response.data, response.data
