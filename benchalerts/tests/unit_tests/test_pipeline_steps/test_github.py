ffe7b5bf73f54ee07a0027768e1ed2c2907a4644
Austin Dickey <austin3spammy@gmail.com>
2023-06-21 10:50:09 -0500

    previous_message

--- benchalerts/tests/unit_tests/test_pipeline_steps/test_github.py
+++ benchalerts/tests/unit_tests/test_pipeline_steps/test_github.py
@@ -25,14 +25,14 @@ class MockAlerter(Alerter):
     def github_check_summary(
         self, full_comparison: FullComparisonInfo, build_url: Optional[str]
     ) -> str:
-        old_message = super().github_check_summary(full_comparison, build_url)
-        return old_message + "\n\nThis message was generated from pytest."
+        previous_message = super().github_check_summary(full_comparison, build_url)
+        return previous_message + "\n\nThis message was generated from pytest."
 
     def github_pr_comment(
         self, full_comparison: FullComparisonInfo, check_link: str
     ) -> str:
-        old_message = super().github_pr_comment(full_comparison, check_link)
-        return old_message + "\n\nThis message was generated from pytest."
+        previous_message = super().github_pr_comment(full_comparison, check_link)
+        return previous_message + "\n\nThis message was generated from pytest."
 
 
 @pytest.mark.parametrize(
