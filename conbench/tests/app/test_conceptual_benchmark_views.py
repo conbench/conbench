7b8814f1fbcd35b5fc1293f0111abd62adb7e6b6
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-08-09 14:52:02 +0200

    wip: c-bench tests

--- conbench/tests/app/test_conceptual_benchmark_views.py
+++ conbench/tests/app/test_conceptual_benchmark_views.py
@@ -1,5 +1,10 @@
+import time
+
 import pytest
 
+import conbench.job
+
+from ...tests.api import _fixtures
 from ...tests.app import _asserts
 
 
@@ -7,6 +12,28 @@ def assert_response_is_login_page(resp):
     assert resp.status_code == 200, (resp.status_code, resp.text)
     assert "<h4>Sign in</h4>" in resp.text, resp.text
     assert '<label for="password">Password</label>' in resp.text, resp.text
+
+
+benchmark_result_dict = {
+    "run_id": "1",
+    "batch_id": "1",
+    "timestamp": "2020-11-25T21:02:44Z",
+    "context": {},
+    # "info": {},
+    # "machine_info": {"foo": "bar"}, this should work, relax constraints
+    "machine_info": _fixtures.MACHINE_INFO,
+    "stats": {
+        "data": [
+            "0.099094",
+            "0.037129",
+        ],
+        "unit": "s",
+    },
+    "tags": {
+        "p1": "v1",
+        "name": "fun-benchmark",
+    },
+}
 
 
 class TestCBenchmarks(_asserts.AppEndpointTest):
@@ -20,3 +47,12 @@ class TestCBenchmarks(_asserts.AppEndpointTest):
         monkeypatch.setenv("BENCHMARKS_DATA_PUBLIC", "off")
         self.logout(client)
         assert_response_is_login_page(client.get(relpath, follow_redirects=True))
+
+    def test_cbench_no_data(self, client):
+        self.authenticate(client)
+        resp = client.post("/api/benchmark-results/", json=benchmark_result_dict)
+        assert resp.status_code == 201, resp.text
+
+        conbench.job.start_jobs()
+        time.sleep(10)
+        conbench.job.stop_jobs_join()
