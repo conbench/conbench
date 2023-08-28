f6e167640bfef139f24cdce01a792570bbca2286
Edward Visel <1693477+alistaire47@users.noreply.github.com>
2023-08-28 11:14:50 -0500

    benchconnect: Remove run posting and BenchmarkRun usage (#1452)
    benchconnect: Remove run posting

--- benchconnect/tests/test_augment.py
+++ benchconnect/tests/test_augment.py
@@ -1,16 +1,13 @@
 import json
 import shutil
 
-import click
 import pytest
 from benchadapt.result import BenchmarkResult
-from benchadapt.run import BenchmarkRun
 from click.testing import CliRunner
 
-from benchconnect._augment import augment_blob, result, run
+from benchconnect._augment import augment_blob, result
 
 minimal_result = {"stats": {"data": [1, 2], "unit": "s"}}
-minimal_run = {}
 
 
 @pytest.fixture
@@ -19,58 +16,45 @@ def mock_github_env_vars(monkeypatch):
     monkeypatch.setenv("CONBENCH_PROJECT_COMMIT", "fake-commit-hash")
 
 
-@pytest.mark.parametrize(
-    ("raw_blob", "cls"),
-    [(minimal_result, BenchmarkResult), (minimal_run, BenchmarkRun)],
-)
-def test_augment_blob(raw_blob: dict, cls, mock_github_env_vars):
-    augmented_blob = augment_blob(raw_blob, cls=cls)
-    assert augmented_blob.keys() > raw_blob.keys()
+def test_augment_blob(mock_github_env_vars):
+    augmented_blob = augment_blob(minimal_result, cls=BenchmarkResult)
+    assert augmented_blob.keys() > minimal_result.keys()
 
 
 class TestCliAugment:
     runner = CliRunner()
 
-    @pytest.mark.parametrize("command", [result, run])
-    def test_help(self, command: click.Command) -> None:
-        res = self.runner.invoke(command, args=["--help"])
+    def test_help(self) -> None:
+        res = self.runner.invoke(result, args=["--help"])
         assert res.exit_code == 0
         assert res.output
 
-    @pytest.mark.parametrize(
-        ("command", "raw_blob"), [(result, minimal_result), (run, minimal_run)]
-    )
-    def test_json(self, command: click.Command, raw_blob: dict, mock_github_env_vars):
-        res = self.runner.invoke(command, args=["--json", json.dumps(raw_blob)])
+    def test_json(self, mock_github_env_vars):
+        res = self.runner.invoke(result, args=["--json", json.dumps(minimal_result)])
         assert res.exit_code == 0
         augmented_blob = json.loads(res.output)
-        assert augmented_blob.keys() > raw_blob.keys()
+        assert augmented_blob.keys() > minimal_result.keys()
 
-    @pytest.mark.parametrize(
-        ("command", "raw_blob"), [(result, minimal_result), (run, minimal_run)]
-    )
-    def test_path(
-        self, command: click.Command, raw_blob: dict, tmpdir, mock_github_env_vars
-    ):
+    def test_path(self, tmpdir, mock_github_env_vars):
         tempjson1 = tmpdir / "file1.json"
         tempjson2 = tmpdir / "file2.json"
 
         for path in [tempjson1, tempjson2]:
             with open(path, "w") as f:
-                json.dump(raw_blob, f)
+                json.dump(minimal_result, f)
 
         # file path
-        res = self.runner.invoke(command, args=["--path", tempjson1])
+        res = self.runner.invoke(result, args=["--path", tempjson1])
         assert res.exit_code == 0
         augmented_blob = json.loads(res.output)
-        assert augmented_blob.keys() > raw_blob.keys()
+        assert augmented_blob.keys() > minimal_result.keys()
 
         # directory path
-        res = self.runner.invoke(command, args=["--path", tmpdir])
+        res = self.runner.invoke(result, args=["--path", tmpdir])
         assert res.exit_code == 0
 
         for blob in res.output.splitlines():
             augmented_blob = json.loads(blob)
-            assert augmented_blob.keys() > raw_blob.keys()
+            assert augmented_blob.keys() > minimal_result.keys()
 
         shutil.rmtree(tmpdir, ignore_errors=True)
