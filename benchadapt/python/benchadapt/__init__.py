6aff31a952d09a3470dea1c9bf0d934e1aabe19c
Edward Visel <1693477+alistaire47@users.noreply.github.com>
2023-08-28 13:39:42 -0500

    benchadapt: Remove BenchmarkRun (#1458)

--- benchadapt/python/benchadapt/__init__.py
+++ benchadapt/python/benchadapt/__init__.py
@@ -1,6 +1,5 @@
 from . import adapters
 from ._version import __version__
 from .result import BenchmarkResult
-from .run import BenchmarkRun
 
-__all__ = ["__version__", "adapters", "BenchmarkResult", "BenchmarkRun"]
+__all__ = ["__version__", "adapters", "BenchmarkResult"]
