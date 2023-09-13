3b24bdd8539653a050be5bc4fdfd1985daa6f349
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-09-13 16:30:47 +0200

    lint

--- ci/screenshot.py
+++ ci/screenshot.py
@@ -32,7 +32,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
 from webdriver_manager.chrome import ChromeDriverManager
 
 # from selenium.webdriver.support.ui import WebDriverWait
